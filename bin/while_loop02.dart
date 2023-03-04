398247f84a4767dd958102b3e65c1867e7c072cb
Sultonbek <idrisovsultonbek@gmail.com>
2023-03-04 20:07:53 +0500

    bin/while_loopo2.dart

--- bin/while_loop02.dart
+++ bin/while_loop02.dart
@@ -2,4 +2,20 @@
   Create function func with "a " argument
   Return the number of even numbers up to "a".
 */
-void main() {}
+
+
+  int func(int a) {
+    int y = 0;
+    int x = 0;
+    while (x <= a) {
+      if (x % 2 == 0) {
+        y += 1;
+      }
+      x++;
+    }
+    return y;
+  }
+
+  void main() {
+    print(func(15));
+  }
