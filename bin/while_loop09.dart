e4004304e78cebbc404f014def4b0f1e3877ec18
Sultonbek <idrisovsultonbek@gmail.com>
2023-03-04 20:09:34 +0500

    bin/while_loopo9.dart

--- bin/while_loop09.dart
+++ bin/while_loop09.dart
@@ -4,4 +4,19 @@
   
   
 */
-void main() {}
+ int func(int a) {
+    int y = 0;
+    int x = 0;
+    while (x <= a) {
+      if (x % 2 == 0) {
+        y += 1;
+      }
+      x++;
+    }
+    return y;
+  }
+
+  void main() {
+    print(func(30));
+  }
+
